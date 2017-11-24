00b7149c199cfdbef0c20e408892606d7f146890
Alexander <ra@ecraft.ru>
2017-11-24 22:13:22 +0300

    Fix nonce for Bitfinex;

--- bitex/api/REST/api.py
+++ bitex/api/REST/api.py
@@ -53,7 +53,7 @@ class APIClient(metaclass=ABCMeta):
         Creates a Nonce value for signature generation
         :return:
         """
-        return str(round(100000 * time.time()) * 2) 
+        return str(round(1000000 * time.time()) * 2)
 
     @staticmethod
     def api_request(*args, **kwargs):
